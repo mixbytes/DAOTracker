f3da21d4ab2690aed2f3805eef42171f37e4dfbb
Random Nerd <randomnerd@icloud.com>
2020-06-02 14:52:15 +0300

    [DPM-254] Add sponsorship flag to trx info

--- src/app/transaction/transaction/information/information.component.ts
+++ src/app/transaction/transaction/information/information.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Serialize } from 'eosjs';
+import { SPONSORSHIP_EXT } from '../../../services/eos.abi'
+import { EosService } from '../../../services/eos.service';
 
 @Component({
   selector: 'app-transaction-information',
@@ -9,9 +12,20 @@ export class InformationComponent implements OnInit {
 
   @Input() transaction;
 
-  constructor() { }
+  constructor(private eosService: EosService) { }
 
   ngOnInit() {
+    for (const { type, data } of this.transaction.trx.trx.transaction_extensions) {
+      if (type !== 0) continue;
+      const sponsorType = Serialize.getTypesFromAbi(this.eosService.api.abiTypes, SPONSORSHIP_EXT).get('sponsor_ext');
+      let buffer = new Serialize.SerialBuffer({
+        textEncoder: new TextEncoder(),
+        textDecoder: new TextDecoder(),
+        array: Serialize.hexToUint8Array(data)
+      });
+      const { sponsor } = sponsorType.deserialize(buffer)
+      this.transaction.sponsored_by = sponsor
+    }
   }
 
 }
