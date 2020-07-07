a49c7cafb687ba481d6f6af862e3e5c06b8bb97b
randomnerd <randomnerd@icloud.com>
2020-07-07 14:37:02 +0300

    fix sorting order

--- src/app/producer/producers/producers.component.ts
+++ src/app/producer/producers/producers.component.ts
@@ -4,7 +4,7 @@ import { EosService } from '../../services/eos.service';
 import { Observable, of, timer } from 'rxjs';
 import { map, share, switchMap, mergeMap } from 'rxjs/operators';
 import {environment} from '../../../environments/environment';
-import { EOSGlobalTableRow, EOSGlobalTable } from '../../interfaces/eos.global.table'
+import { EOSGlobalTableRow, EOSGlobalTable } from '../../interfaces/eos.global.table';
 
 @Component({
   templateUrl: './producers.component.html',
@@ -15,7 +15,7 @@ export class ProducersComponent implements OnInit {
   columnHeaders$: Observable<string[]> = of(PRODUCERS_COLUMNS);
   producers$: Observable<any[]>;
   chainStatus$: Observable<any>;
-  private EOSGlobalTable: EOSGlobalTable
+  private EOSGlobalTable: EOSGlobalTable;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
@@ -23,19 +23,21 @@ export class ProducersComponent implements OnInit {
   ) { }
 
   async getGlobalTableContent(): Promise<EOSGlobalTableRow> {
-    let table = this.EOSGlobalTable
-    if (!table) table = await this.eosService.eos.get_table_rows({
-      json: true,
-      code: "eosio",
-      scope: "eosio",
-      table: "global"
-    })
-    return table && table.rows && table.rows.length ? table.rows[0] : null
+    let table = this.EOSGlobalTable;
+    if (!table) {
+      table = await this.eosService.eos.get_table_rows({
+        json: true,
+        code: 'eosio',
+        scope: 'eosio',
+        table: 'global'
+      });
+    }
+    return table && table.rows && table.rows.length ? table.rows[0] : null;
   }
 
   async countActiveProducers() {
     const globalTable = await this.getGlobalTableContent();
-    return globalTable.target_producer_schedule_size || 21
+    return globalTable.target_producer_schedule_size || 21;
   }
 
   async countRewards(total_votes, index, totalProducerVoteWeight, votesToRemove) {
@@ -43,16 +45,14 @@ export class ProducersComponent implements OnInit {
     const position = index;
     const percentageVotesRewarded = total_votes / (totalProducerVoteWeight - votesToRemove) * 100;
 
-    if (position < await this.countActiveProducers()) reward += 443;
+    if (position < await this.countActiveProducers()) { reward += 443; }
     reward += percentageVotesRewarded * 200;
-    if (reward < 100) reward = 0;
+    if (reward < 100) { reward = 0; }
 
     return Math.floor(reward);
   }
 
   ngOnInit() {
-    let activeCount = 21;
-    this.countActiveProducers().then(n => activeCount = n)
     this.columnHeaders$ = this.breakpointObserver.observe(Breakpoints.XSmall).pipe(
       map(result => result.matches ? PRODUCERS_COLUMNS.filter((c: any) => (c !== 'url' && c !== 'numVotes')) : PRODUCERS_COLUMNS)
     );
@@ -65,15 +65,26 @@ export class ProducersComponent implements OnInit {
         mergeMap(async producers => {
           const votesToRemove = producers.reduce((acc, cur) => {
             const percentageVotes = cur.total_votes / chainStatus.total_producer_vote_weight * 100;
-            if (percentageVotes * 200 < 100) acc += parseFloat(cur.total_votes);
+            if (percentageVotes * 200 < 100) { acc += parseFloat(cur.total_votes); }
             return acc;
           }, 0);
-          const activeCount = await this.countActiveProducers()
-          return producers.map((producer, index) => {
+          const activeCount = await this.countActiveProducers();
+          return producers
+              .sort((producerA, producerB) => {
+                const activeA = parseInt(producerA.is_active, 10);
+                const activeB = parseInt(producerB.is_active, 10);
+                return activeA === activeB
+                    ? 0
+                    : activeA > activeB
+                      ? -1
+                      : 1;
+              })
+              .map((producer, index) => {
             let reward = 0;
-            const position = parseInt(index) + 1;
+            const position = parseInt(index, 10) + 1;
+            console.log(producer.owner, producer.is_active, position);
             const active = producer.is_active && position <= activeCount;
-            const numVotes = (producer.total_votes / this.calculateVoteWeight() / 10000).toFixed(0)
+            const numVotes = (producer.total_votes / this.calculateVoteWeight() / 10000).toFixed(0);
             const votes = (producer.total_votes / chainStatus.total_producer_vote_weight * 100).toFixed(2);
 
             if (environment.token === 'TLOS') {
@@ -88,10 +99,10 @@ export class ProducersComponent implements OnInit {
                 index,
                 chainStatus.total_producer_vote_weight,
                 votesToRemove
-              ) as any
+              ) as any;
             }
 
-            return { ...producer, position, reward, votes, numVotes, active }
+            return { ...producer, position, reward, votes, numVotes, active };
           });
         })
       )),
@@ -100,13 +111,13 @@ export class ProducersComponent implements OnInit {
   }
 
   private calculateVoteWeight() {
-    //time epoch:
-    //https://github.com/EOSIO/eos/blob/master/contracts/eosiolib/time.hpp#L160
-    //stake to vote
-    //https://github.com/EOSIO/eos/blob/master/contracts/eosio.system/voting.cpp#L105-L109
-    let timestamp_epoch: number = 946684800000;
-    let dates_: number = (Date.now() / 1000) - (timestamp_epoch / 1000);
-    let weight_: number = Math.floor(dates_ / (86400 * 7)) / 52;  //86400 = seconds per day 24*3600
+    // time epoch:
+    // https://github.com/EOSIO/eos/blob/master/contracts/eosiolib/time.hpp#L160
+    // stake to vote
+    // https://github.com/EOSIO/eos/blob/master/contracts/eosio.system/voting.cpp#L105-L109
+    const timestamp_epoch = 946684800000;
+    const dates_: number = (Date.now() / 1000) - (timestamp_epoch / 1000);
+    const weight_: number = Math.floor(dates_ / (86400 * 7)) / 52;  // 86400 = seconds per day 24*3600
     return Math.pow(2, weight_);
   }
 
